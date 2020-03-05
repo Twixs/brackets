44179716f6b9d8177aae8048e3e05d2c86a24666
Alina <alina.kuzzzovkova@gmail.com>
2020-03-05 22:43:44 +0200

    added solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let array = [];
+  const checkBracketOpen = (bracket) => {
+    return !!bracketsConfig.find(elem => elem[0] === bracket);
+  }
+  const checkBracketCLosed = (bracket, prevOpen) => {
+    const pair = bracketsConfig.find(elem => elem[1] === bracket);
+    return pair[0] !== prevOpen;
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (checkBracketOpen(str[i])) {
+      const pair = bracketsConfig.find(elem => elem[0] === str[i]);
+      if (str[i] === pair[0] && str[i] === pair[1] && str[i] === array[array.length-1]) {
+        array.pop();
+      } else {
+        array.push(str[i]);
+      }
+    } else {
+      if (checkBracketCLosed(str[i], array[array.length-1])) {
+        return false;
+      }
+      array.pop();
+    }
+  }
+  return array.length > 0 ? false : true;
 }
